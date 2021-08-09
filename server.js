f018eea9e71a03f1976dc1eb38f152c30191556b
ssliteris <ssliteris@exadel.com>
2021-08-09 11:48:05 +0300

    fix/build-path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/ajax-nds'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/' }),
+  res.sendFile('index.html', { root: 'dist/ajax-nds/' }),
 );
 
 app.listen(process.env.PORT || 8080);
